f0b02c19862bcedb17780d397c95b50c024dea68
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-04-29 11:59:12 +1200

    updated camera script so easier changing of camera zoom

--- A Mafia Among Us/Assets/Scripts/cameraFollow.cs
+++ A Mafia Among Us/Assets/Scripts/cameraFollow.cs
@@ -6,7 +6,11 @@ public class cameraFollow : MonoBehaviour
 {
     
     public Transform playerCharacter;
-    
+    public float cameraDistance = 30.0f;
+
+    void awake(){
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height /2) / cameraDistance);
+    }
     void FixedUpdate(){
 
         transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, -10);
