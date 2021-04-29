1ec87cf7b29e427a26c9a3cd870339334d78a156
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-04-29 13:19:39 +1200

    Camera changed so that is always in bounds of the map

--- A Mafia Among Us/Assets/Scripts/cameraFollow.cs
+++ A Mafia Among Us/Assets/Scripts/cameraFollow.cs
@@ -7,12 +7,38 @@ public class cameraFollow : MonoBehaviour
     
     public Transform playerCharacter;
     public float cameraDistance = 30.0f;
+    public float playerx;
+    public float playery;
 
     void awake(){
         GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height /2) / cameraDistance);
     }
     void FixedUpdate(){
 
-        transform.position = new Vector3(playerCharacter.position.x, playerCharacter.position.y, -10);
+        if(playerCharacter.position.y>23){
+            playery = 23;
+        }
+        else if(playerCharacter.position.y<-139){
+            playery = -139;
+        }
+        else
+        {
+            playery = playerCharacter.position.y;
+            
+        }
+
+        if(playerCharacter.position.x<-190){
+            playerx = -190;
+        }
+        else if(playerCharacter.position.x>107){
+            playerx = 107;
+        }
+        else
+        {
+            playerx = playerCharacter.position.x;
+            
+        }
+
+        transform.position = new Vector3(playerx, playery , -5);
     }
 }
