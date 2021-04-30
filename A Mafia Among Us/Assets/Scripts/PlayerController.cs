3f814b5913959da2ee1ca8dbac9bf77582f451ec
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-04-30 16:19:55 +1200

    All Player Functionality Finalized

--- A Mafia Among Us/Assets/Scripts/PlayerController.cs
+++ A Mafia Among Us/Assets/Scripts/PlayerController.cs
@@ -14,14 +14,28 @@ public class PlayerController : MonoBehaviour
 
     Vector2 movementInput;
 
-    [SerializeField] bool isImposter;
-    [SerializeField] InputAction KILL;
-
     static Color myColour;
     SpriteRenderer myAvatarSprite;
 
     [SerializeField] bool hasControl;
     public static PlayerController localPlayer;
+
+    [SerializeField] bool isImposter;
+    [SerializeField] InputAction KILL;
+
+    List<PlayerController> targets;
+    [SerializeField] Collider myCollider;
+
+    bool isDead;
+
+    [SerializeField] GameObject bodyPrefab;
+
+
+
+    private void Awake()
+    {
+        KILL.performed += KILLTarget;
+    }
 
 
     private void OnEnable()
@@ -43,6 +57,8 @@ public class PlayerController : MonoBehaviour
             localPlayer = this;
         }
 
+        targets = new List<PlayerController>();
+
         myRb = GetComponent<Rigidbody>();
         myAvatar = transform.GetChild(0);
 
@@ -51,12 +67,23 @@ public class PlayerController : MonoBehaviour
         myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
         if (myColour == Color.clear)
             myColour = Color.white;
-        myAvatarSprite.color = myColour;
+        if (hasControl)
+        {
+            myAvatarSprite.color = myColour;
+        }
+        else
+            return;
+
+        
     }
 
 
     void Update()
     {
+        if (!hasControl)
+        {
+            return;
+        }
         movementInput = WASD.ReadValue<Vector2>();
 
         if(movementInput.x != 0)
@@ -79,6 +106,73 @@ public class PlayerController : MonoBehaviour
         {
             myAvatarSprite.color = myColour;
         }
+    }
+
+    public void SetRole(bool newRole)
+    {
+        isImposter = newRole;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController tempTarget = other.GetComponent<PlayerController>();
+            if (isImposter)
+            {
+                if (tempTarget.isImposter)
+                    return;
+                else
+                {
+                    targets.Add(tempTarget);
+                    //Debug.Log(target.name);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController tempTarget = other.GetComponent<PlayerController>();
+            if (targets.Contains(tempTarget))
+            {
+                targets.Remove(tempTarget);
+            }
+        }
+    }
+
+    void KILLTarget(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            if (targets.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                if (targets[targets.Count - 1].isDead)
+                    return;
+
+                transform.position = targets[targets.Count - 1].transform.position;
+                targets[targets.Count - 1].Die();
+                targets.RemoveAt(targets.Count - 1);
+
+            }
+        }
+    }
+
+    public void Die()
+    {
+
+        isDead = true;
+        myAnimator.SetBool("IsDead", isDead);
+        myCollider.enabled = false;
+
+        DeadBody tempBody = Instantiate(bodyPrefab, transform.position, transform.rotation).GetComponent<DeadBody>();
+        tempBody.SetColor(myAvatarSprite.color);
     }
 
 }
