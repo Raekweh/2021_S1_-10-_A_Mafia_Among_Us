6632a81f715f2174d463c87fc4dff75455e7eaa4
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-04-30 23:23:45 +1200

    Update AU_CharacterCustomizer.cs

--- Sprint1Project/Sprint1/Assets/Scripts/AU_CharacterCustomizer.cs
+++ Sprint1Project/Sprint1/Assets/Scripts/AU_CharacterCustomizer.cs
@@ -5,12 +5,15 @@ using UnityEngine.SceneManagement;
 public class AU_CharacterCustomizer : MonoBehaviour
 {
     [SerializeField] Color[] allColors;
+
+    //This method is used to set the player's colour which the user is controlling
     public void SetColor(int colorIndex)
     {
         AU_PlayerController.localPlayer.SetColor(allColors[colorIndex]);
     }
+
+    //This method is used to move to the next scene in the build
     public void NextScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
-}
