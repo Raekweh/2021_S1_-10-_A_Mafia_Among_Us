4f6b635fd28edbe0f7b1173553fea7d8679f6aa1
xpg3393 <73453814+xpg3393@users.noreply.github.com>
2021-04-30 23:23:03 +1200

    Update AU_Body.cs

--- Sprint1Project/Sprint1/Assets/Scripts/AU_Body.cs
+++ Sprint1Project/Sprint1/Assets/Scripts/AU_Body.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 public class AU_Body : MonoBehaviour
 {
     [SerializeField] SpriteRenderer bodySprite;
+
+    //This method sets the colour of the body to newColor
     public void SetColor(Color newColor)
     {
         bodySprite.color = newColor;
     }
+
+    //This methodd enables the deadbody by adding the transform to the player
     private void OnEnable()
     {
         if (AU_PlayerController.allBodies != null)
-        {
-            AU_PlayerController.allBodies.Add(transform);
+@ -15,6 + 19,8 @@ public class AU_Body : MonoBehaviour
+ 
+             AU_PlayerController.allBodies.Add(transform);
         }
     }
+
+    //This method removes the dead sprite of the dead character and notes to the debug log that the body has been reported
     public void Report()
-    {
-        Debug.Log("Reported");
-        Destroy(gameObject);
-    }
-}
+{
+    Debug.Log("Reported");
+@ @@
