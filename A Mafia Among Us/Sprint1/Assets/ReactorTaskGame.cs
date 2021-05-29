2bde66d1f18b0c7e7d9500f06f2cc3986c7fa9a2
sjs4793 <73675052+sjs4793@users.noreply.github.com>
2021-05-30 03:10:41 +1200

    Reactor Task - Script Added

--- A Mafia Among Us/Sprint1/Assets/ReactorTaskGame.cs
+++ A Mafia Among Us/Sprint1/Assets/ReactorTaskGame.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class ReactorTaskGame : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int nextButton;
+    [SerializeField] GameObject GamePanel;
+    [SerializeField] GameObject[] myObjects;
+
+
     void Start()
     {
-        
+        nextButton = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        nextButton = 0;
+        for(int i = 0; i < myObjects.Length; i++)
+        {
+            myObjects[i].transform.SetSiblingIndex(Random.Range(0, 9));
+        }
+    }
+
+    public void ButtonOrder(int button)
+    {
+        Debug.Log("Pressed");
+        if (button == nextButton)
+        {
+            nextButton++;
+            Debug.Log("Next Button" + nextButton);
+        }
+        else
+        {
+            Debug.Log("Failed");
+            Debug.Log("Next Button" + nextButton);
+            nextButton = 0;
+            OnEnable();
+        }
+        if (button == 9)
+        {
+            Debug.Log("Pass");
+            nextButton = 0;
+            ButtonOrderPanelClose();
+        }
+    }
+
+    public void ButtonOrderPanelClose()
+    {
+        GamePanel.SetActive(false);
+    }
+    public void ButtonOrderPanelOpen()
+    {
+        GamePanel.SetActive(true);
     }
 }
