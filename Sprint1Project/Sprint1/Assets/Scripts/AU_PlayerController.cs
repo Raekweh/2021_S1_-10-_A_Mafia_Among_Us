72d4675904c49b8d25cd427167197dbc49124e0c
pharaaz <80433979+pharaaz@users.noreply.github.com>
2021-05-31 21:05:45 +1200

    code grab

--- Sprint1Project/Sprint1/Assets/Scripts/AU_PlayerController.cs
+++ Sprint1Project/Sprint1/Assets/Scripts/AU_PlayerController.cs
@@ -1,134 +1,159 @@
-using System;
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class AU_PlayerController : MonoBehaviour
+
+public class AU_PlayerController : MonoBehaviour, IPunObservable
 {
     [SerializeField] bool hasControl;
     public static AU_PlayerController localPlayer;
-
-    //key used to inspect player
-    bool isInspectAni;
-
+    
     //Components
     Rigidbody myRB;
     Animator myAnim;
     Transform myAvatar;
+
     //Player movement
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
+
+    float direction = 1;
     //Player Color
-    [SerializeField] Color myColor;
-    SpriteRenderer myAvatarSprite;
+    static Color myColor;
+    Vector3 colorVector;
+    Color syncColor;
+    [SerializeField] SpriteRenderer myAvatarSprite;
+
     //Role
-
-    [SerializeField] bool isAngel;
-    [SerializeField] InputAction REVIVE;
-    float reviveInput;
-
-    [SerializeField] bool isInspector;
-    [SerializeField] InputAction INSPECTT;
-    float inspecttInput;
-
-
-
     [SerializeField] bool isImposter;
+    static int imposterNumber;
+    static bool imposterNumberAssigned;
+    static bool imposterAssigned;
     [SerializeField] InputAction KILL;
     float killInput;
     List<AU_PlayerController> targets;
     [SerializeField] Collider myCollider;
     bool isDead;
-    
     [SerializeField] GameObject bodyPrefab;
     public static List<Transform> allBodies;
     List<Transform> bodiesFound;
     [SerializeField] InputAction REPORT;
     [SerializeField] LayerMask ignoreForBody;
 
+    //Interaction
+    [SerializeField] InputAction MOUSE;
+    Vector2 mousePositionInput;
+    Camera myCamera;
+    [SerializeField] InputAction INTERACTION;
+    [SerializeField] LayerMask interactLayer;
+    
+    //Networking
+    PhotonView myPV;
+    [SerializeField] GameObject lightMask;
     private void Awake()
     {
         KILL.performed += KillTarget;
-        REPORT.performed += ReportBody;
-        REVIVE.performed += ReviveTarget;
-        INSPECTT.performed += InspecttTarget;
+        INTERACTION.performed += Interact;
     }
-
-    //This method enables the movement input, killing and reporting functionality
     private void OnEnable()
     {
         WASD.Enable();
         KILL.Enable();
         REPORT.Enable();
-        REVIVE.Enable();
-        INSPECTT.Enable();
+        MOUSE.Enable();
+        INTERACTION.Enable();
     }
-
-    //This method disables the movement input, killing and reporting functionality
     private void OnDisable()
     {
         WASD.Disable();
         KILL.Disable();
         REPORT.Disable();
-        REVIVE.Disable();
-        INSPECTT.Disable();
+        MOUSE.Disable();
+        INTERACTION.Disable();
     }
-
     // Start is called before the first frame update
     void Start()
     {
-        if (hasControl)
+        myPV = GetComponent<PhotonView>();
+
+        if(myPV.IsMine)
         {
             localPlayer = this;
+            Debug.Log("Player localised");
         }
-
+        myCamera = transform.GetChild(1).GetComponent<Camera>();
         targets = new List<AU_PlayerController>();
         myRB = GetComponent<Rigidbody>();
         myAnim = GetComponent<Animator>();
         myAvatar = transform.GetChild(0);
         myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
-        if (!hasControl)
+        if (!myPV.IsMine)
+        {
+            myCamera.gameObject.SetActive(false);
+            lightMask.SetActive(false);
             return;
+        }
         if (myColor == Color.clear)
             myColor = Color.white;
         myAvatarSprite.color = myColor;
-
-        allBodies = new List<Transform>();
+        if(allBodies == null)
+        {
+            allBodies = new List<Transform>();
+        }
         bodiesFound = new List<Transform>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (!hasControl)
+        // Debug.Log(imposterNumberAssigned);
+        if(!isImposter && imposterNumberAssigned && !imposterAssigned){
+            BecomeImposter(imposterNumber);
+            imposterAssigned = true;
+        }
+        // Debug.Log("this = "+this);
+        // Debug.Log("Is imposter = "+this.isImposter);
+
+        myAvatar.localScale = new Vector2(direction, 1);
+
+        if (!myPV.IsMine){
+            myAvatarSprite.color = syncColor;
             return;
+        }
+
         movementInput = WASD.ReadValue<Vector2>();
         myAnim.SetFloat("Speed", movementInput.magnitude);
+
         if (movementInput.x != 0)
         {
-            myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
-            isInspectAni = false;
-            myAnim.SetBool("isInspectAni", isInspectAni);
+            direction = Mathf.Sign(movementInput.x);
+            
         }
-        if (movementInput.y != 0)
-        {
-            isInspectAni = false;
-            myAnim.SetBool("isInspectAni", isInspectAni);
-        }
-        if (allBodies.Count > 0)
+        
+        if(allBodies.Count > 0)
         {
             BodySearch();
         }
+        if(REPORT.triggered)
+        {
+            if (bodiesFound.Count == 0)
+                return;
+            Transform tempBody = bodiesFound[bodiesFound.Count - 1];
+            allBodies.Remove(tempBody);
+            bodiesFound.Remove(tempBody);
+            tempBody.GetComponent<AU_Body>().Report();
+        }
+        mousePositionInput = MOUSE.ReadValue<Vector2>();
+        
     }
-
-    //updates the velocity of the rigidbody aka character
     private void FixedUpdate()
     {
+        if (!myPV.IsMine)
+            return;
         myRB.velocity = movementInput * movementSpeed;
     }
-
-    //sets the colour of the avatar sprite to newColor
     public void SetColor(Color newColor)
     {
         myColor = newColor;
@@ -137,16 +162,10 @@ public class AU_PlayerController : MonoBehaviour
             myAvatarSprite.color = myColor;
         }
     }
-
-    //sets the players role to imposter or not imposter
     public void SetRole(bool newRole)
     {
         isImposter = newRole;
-        isAngel = newRole;
-        isInspector = newRole;
     }
-
-    //this method adds potential murder targers to a target array when they enter the range
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -159,33 +178,11 @@ public class AU_PlayerController : MonoBehaviour
                 else
                 {
                     targets.Add(tempTarget);
-
-                }
-            }
-            if (isAngel)
-            {
-                if (tempTarget.isDead != false)
-                    return;
-                else
-                {
-                    targets.Add(tempTarget);
-
-                }
-            }
-            if (isInspector)
-            {
-                if (tempTarget.isImposter)
-                    targets.Add(tempTarget);
-                else
-                {
-                    return;
-
+                    
                 }
             }
         }
     }
-
-    //this method rtemoves the potential murder targers from the target array when they leave the range
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
@@ -193,18 +190,22 @@ public class AU_PlayerController : MonoBehaviour
             AU_PlayerController tempTarget = other.GetComponent<AU_PlayerController>();
             if (targets.Contains(tempTarget))
             {
-                targets.Remove(tempTarget);
+                    targets.Remove(tempTarget);
             }
         }
     }
-
-    //This method will kill the last player to enter the imposters kill radius
-    private void KillTarget(InputAction.CallbackContext context)
+    void KillTarget(InputAction.CallbackContext context)
     {
+
+        if(!myPV.IsMine){
+            return;
+        }
+        if(!isImposter){
+            return;
+        }
+
         if (context.phase == InputActionPhase.Performed)
         {
-            
-            //Debug.Log(targets.Count);
             if (targets.Count == 0)
                 return;
             else
@@ -212,122 +213,111 @@ public class AU_PlayerController : MonoBehaviour
                 if (targets[targets.Count - 1].isDead)
                     return;
                 transform.position = targets[targets.Count - 1].transform.position;
-                targets[targets.Count - 1].Die();
+                // targets[targets.Count - 1].Die();
+                targets[targets.Count - 1].myPV.RPC("RPC_Kill", RpcTarget.All);
                 targets.RemoveAt(targets.Count - 1);
             }
         }
     }
 
-    //This method will Revive the last player to enter the Angels kill radius
-    private void ReviveTarget(InputAction.CallbackContext context)
+    [PunRPC]
+    void RPC_Kill()
     {
-        if (context.phase == InputActionPhase.Performed)
-        {
-            //Debug.Log(targets.Count);
-            if (targets.Count == 0)
-                return;
-            else
-            {
-                if (targets[targets.Count - 1].isDead == false)
-                    return;
-                transform.position = targets[targets.Count - 1].transform.position;
-                targets[targets.Count - 1].Backy();
-                // targets.RemoveAt(targets.Count - 1);
-            }
-        }
+        Die();
     }
 
-    //This method will Revive the last player to enter the Angels kill radius
-    private void InspecttTarget(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Performed)
-        {
-            //Debug.Log(targets.Count);
-            if (targets.Count == 0)
-                return;
-            else
-            {
-
-                if (targets[targets.Count - 1].isImposter)
-                {
-                    return;
-                }
-                transform.position = targets[targets.Count - 1].transform.position;
-
-                 targets[targets.Count - 1].Inspectyy();
-               
-            }
-        }
-    }
-
-    //this method will inspect the player to see if they are the mafia
-    public void Inspectyy()
-    {
-        isInspectAni = true;
-        myAnim.SetBool("isInspectAni", isInspectAni);
-        gameObject.layer = 9;
-       // myCollider.enabled = true;
-    }
-
-    //this method will cause the player to revive
-    public void Backy()
-    {
-        //AU_Body tempBody = Instantiate(bodyPrefab, transform.position, transform.rotation).GetComponent<AU_Body>();
-        //watempBody.SetColor(myAvatarSprite.color);
-        isDead = false;
-        myAnim.SetBool("IsDead", isDead);
-        gameObject.layer = 9;
-        myCollider.enabled = true;
-    }
-
-
-    //this method will cause the plauyer to die leaving a dead sprite at the location of death
     public void Die()
     {
-        AU_Body tempBody = Instantiate(bodyPrefab, transform.position, transform.rotation).GetComponent<AU_Body>();
+        if (!myPV.IsMine)
+        {
+            return;
+        }
+        AU_Body tempBody = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "AU_Body"), transform.position, transform.rotation).GetComponent<AU_Body>();
         tempBody.SetColor(myAvatarSprite.color);
         isDead = true;
         myAnim.SetBool("IsDead", isDead);
         gameObject.layer = 9;
         myCollider.enabled = false;
     }
-
-    //This method searches for bodies by calculating rays between the player and a body
     void BodySearch()
     {
-        foreach (Transform body in allBodies)
+        foreach(Transform body in allBodies)
         {
             RaycastHit hit;
             Ray ray = new Ray(transform.position, body.position - transform.position);
             Debug.DrawRay(transform.position, body.position - transform.position, Color.cyan);
-            if (Physics.Raycast(ray, out hit, 1000f, ~ignoreForBody))
+            if(Physics.Raycast(ray, out hit, 1000f, ~ignoreForBody))
             {
-
+                
                 if (hit.transform == body)
                 {
+                    Debug.Log(hit.transform.name);
+                    Debug.Log(bodiesFound.Count);
                     if (bodiesFound.Contains(body.transform))
                         return;
                     bodiesFound.Add(body.transform);
                 }
                 else
                 {
-
+                    
                     bodiesFound.Remove(body.transform);
                 }
             }
         }
     }
-
-    //this method is used to report the body when they are in the range of a player
-    private void ReportBody(InputAction.CallbackContext obj)
+    void Interact(InputAction.CallbackContext context)
     {
-        if (bodiesFound == null)
-            return;
-        if (bodiesFound.Count == 0)
-            return;
-        Transform tempBody = bodiesFound[bodiesFound.Count - 1];
-        allBodies.Remove(tempBody);
-        bodiesFound.Remove(tempBody);
-        tempBody.GetComponent<AU_Body>().Report();
+        if (context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("Here");
+            RaycastHit hit;
+            Ray ray = myCamera.ScreenPointToRay(mousePositionInput);
+            if (Physics.Raycast(ray, out hit,interactLayer))
+            {
+                if (hit.transform.tag == "Interactable")
+                {
+                    // AU_Interactable temp = hit.transform.GetComponent<AU_Interactable>();
+                    // temp.PlayMiniGame();
+                }
+            }
+           
+        }
+        
     }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(direction);
+            stream.SendNext(isImposter);
+            colorVector = new Vector3(myColor.r, myColor.g, myColor.b);
+            stream.SendNext(colorVector);
+        }
+        else
+        {
+            this.direction = (float)stream.ReceiveNext();
+            this.isImposter = (bool)stream.ReceiveNext();
+            this.colorVector = (Vector3)stream.ReceiveNext();
+            syncColor = new Color(colorVector.x, colorVector.y, colorVector.z, 1.0f);
+        }
+    }
+
+    public void BecomeImposter(int ImposterNumber)
+    {
+        if(PhotonNetwork.LocalPlayer == PhotonNetwork.PlayerList[ImposterNumber])
+        {
+            
+            this.isImposter = true;
+            Debug.Log("this = "+localPlayer);
+            Debug.Log("Is imposter ="+isImposter);
+
+        }
+    }
+
+    public void setImposterNumber(int ImposterNumber){
+        imposterNumber = ImposterNumber;
+        imposterNumberAssigned = true;
+    }
+    
 }
