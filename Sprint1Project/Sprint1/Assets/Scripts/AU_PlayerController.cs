ba7fe139b0101ec4a5ac5c637c94c5aaaa7f84a2
pharaaz <80433979+pharaaz@users.noreply.github.com>
2021-06-03 15:10:07 +1200

    inspecc

--- Sprint1Project/Sprint1/Assets/Scripts/AU_PlayerController.cs
+++ Sprint1Project/Sprint1/Assets/Scripts/AU_PlayerController.cs
@@ -16,7 +16,7 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     public static AU_PlayerController localPlayer;
 
     public int tasksCompleted = 0;
-    
+
     //Components
     Rigidbody myRB;
     Animator myAnim;
@@ -41,7 +41,10 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     [SerializeField] bool isImposter;
     static int imposterNumber;
     static bool imposterAssigned;
-
+    [SerializeField] bool isInspector;
+    [SerializeField] InputAction INSPECTT;
+    float inspecttInput;
+    bool isInspectAni;
     [SerializeField] InputAction KILL;
     float killInput;
     List<AU_PlayerController> targets;
@@ -59,19 +62,20 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     Camera myCamera;
     [SerializeField] InputAction INTERACTION;
     [SerializeField] LayerMask interactLayer;
-    
+
     //Networking
     public PhotonView myPV;
     [SerializeField] GameObject lightMask;
 
     public static List<AU_PlayerController> playersInGame;
     public static List<string> playerNames;
-    
+
     private void Awake()
     {
         KILL.performed += KillTarget;
         INTERACTION.performed += Interact;
         REVIVE.performed += ReviveTarget;
+        INSPECTT.performed += InspecttTarget;
     }
     private void OnEnable()
     {
@@ -81,6 +85,7 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
         MOUSE.Enable();
         INTERACTION.Enable();
         REVIVE.Enable();
+        INSPECTT.Enable();
     }
     private void OnDisable()
     {
@@ -90,13 +95,14 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
         MOUSE.Disable();
         INTERACTION.Disable();
         REVIVE.Disable();
+        INSPECTT.Disable();
     }
     // Start is called before the first frame update
     void Start()
     {
         myPV = GetComponent<PhotonView>();
 
-        if(myPV.IsMine)
+        if (myPV.IsMine)
         {
             localPlayer = this;
             Debug.Log("Player localised");
@@ -121,7 +127,7 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
         if (myColor == Color.clear)
             myColor = Color.white;
         myAvatarSprite.color = myColor;
-        if(allBodies == null)
+        if (allBodies == null)
         {
             allBodies = new List<Transform>();
         }
@@ -134,21 +140,24 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     // Update is called once per frame
     void Update()
     {
-        if((playersInGame!=null && this !=null) && !playersInGame.Contains(this)){
+        if ((playersInGame != null && this != null) && !playersInGame.Contains(this))
+        {
             playersInGame.Add(this);
         }
 
-        Debug.Log("Tasks done = "+tasksCompleted);
+        Debug.Log("Tasks done = " + tasksCompleted);
 
         BecomeImposter(imposterNumber);
 
-        if(checkCrewmateVictory()){
+        if (checkCrewmateVictory())
+        {
             Debug.Log("Crewmates Win!");
             PhotonNetwork.AutomaticallySyncScene = true;
             PhotonNetwork.LoadLevel(3);
         }
 
-        if(checkMafiaVictory()){
+        if (checkMafiaVictory())
+        {
             Debug.Log("Mafia Wins!");
             PhotonNetwork.AutomaticallySyncScene = true;
             PhotonNetwork.LoadLevel(4);
@@ -156,7 +165,8 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
 
         myAvatar.localScale = new Vector2(direction, 1);
 
-        if (!myPV.IsMine){
+        if (!myPV.IsMine)
+        {
             myAvatarSprite.color = syncColor;
             return;
         }
@@ -168,13 +178,13 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
 
         if (SceneManager.GetActiveScene().buildIndex == 2)
         {
-            
+
             if (myPV.IsMine)
             {
                 this.myCamera.enabled = true;
             }
-            
-            
+
+
         }
 
         movementInput = WASD.ReadValue<Vector2>();
@@ -183,14 +193,20 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
         if (movementInput.x != 0)
         {
             direction = Mathf.Sign(movementInput.x);
+            isInspectAni = false;
+            myAnim.SetBool("isInspectAni", isInspectAni);
         }
-        
-        if(allBodies.Count > 0)
+        if (movementInput.y != 0)
+        {
+            isInspectAni = false;
+            myAnim.SetBool("isInspectAni", isInspectAni);
+        }
+        if (allBodies.Count > 0)
         {
             BodySearch();
         }
 
-        if(REPORT.triggered)
+        if (REPORT.triggered)
         {
             if (bodiesFound.Count == 0)
                 return;
@@ -199,7 +215,7 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
             bodiesFound.Remove(tempBody);
             tempBody.GetComponent<AU_Body>().Report();
         }
-        mousePositionInput = MOUSE.ReadValue<Vector2>();        
+        mousePositionInput = MOUSE.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
@@ -222,6 +238,8 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     public void SetRole(bool newRole)
     {
         isImposter = newRole;
+        isAngel = newRole;
+        isInspector = newRole;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -235,10 +253,11 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
                     return;
                 else
                 {
-                    if(targets!=null){
+                    if (targets != null)
+                    {
                         targets.Add(tempTarget);
                     }
-                    
+
 
                 }
             }
@@ -253,6 +272,16 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
 
                 //}
             }
+            if (isInspector)
+            {
+                if (tempTarget.isImposter)
+                    targets.Add(tempTarget);
+                else
+                {
+                    return;
+
+                }
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -262,9 +291,47 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
             AU_PlayerController tempTarget = other.GetComponent<AU_PlayerController>();
             if (targets.Contains(tempTarget))
             {
-                    targets.Remove(tempTarget);
+                targets.Remove(tempTarget);
             }
         }
+    }
+
+    private void InspecttTarget(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            //Debug.Log(targets.Count);
+            if (targets.Count == 0)
+                return;
+            else
+            {
+
+                if (targets[targets.Count - 1].isImposter)
+                {
+                    return;
+                }
+                transform.position = targets[targets.Count - 1].transform.position;
+
+                //Thread worker1 = new Thread(targets[targets.Count - 1].Inspectyy());
+                // Thread worker2 = new Thread(targets[targets.Count - 1].changeInpect());
+
+                // worker1.Start();
+                // worker2.Start();
+
+                targets[targets.Count - 1].myPV.RPC("RPC_Inspec", RpcTarget.All);
+                // targets[targets.Count - 1].Inspectyy();
+                // targets[targets.Count - 1].changeInpect();
+                // targets.RemoveAt(targets.Count - 1);
+            }
+        }
+    }
+
+    public void Inspectyy()
+    {
+        isInspectAni = true;
+        myAnim.SetBool("isInspectAni", isInspectAni);
+        gameObject.layer = 9;
+        // myCollider.enabled = true;
     }
 
     private void ReviveTarget(InputAction.CallbackContext context)
@@ -300,10 +367,12 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     void KillTarget(InputAction.CallbackContext context)
     {
 
-        if(!myPV.IsMine){
+        if (!myPV.IsMine)
+        {
             return;
         }
-        if(!isImposter){
+        if (!isImposter)
+        {
             return;
         }
 
@@ -324,10 +393,15 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     }
 
     [PunRPC]
+    void RPC_Inspec()
+    {
+        Inspectyy();
+    }
+
+    [PunRPC]
     void RPC_Revive()
     {
         Revivve();
-        // Die();
     }
 
     [PunRPC]
@@ -367,14 +441,14 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     }
     void BodySearch()
     {
-        foreach(Transform body in allBodies)
+        foreach (Transform body in allBodies)
         {
             RaycastHit hit;
             Ray ray = new Ray(transform.position, body.position - transform.position);
             Debug.DrawRay(transform.position, body.position - transform.position, Color.cyan);
-            if(Physics.Raycast(ray, out hit, 1000f, ~ignoreForBody))
+            if (Physics.Raycast(ray, out hit, 1000f, ~ignoreForBody))
             {
-                
+
                 if (hit.transform == body)
                 {
                     Debug.Log(hit.transform.name);
@@ -391,7 +465,7 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
             }
         }
     }
-    
+
     void Interact(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
@@ -399,18 +473,18 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
             //Debug.Log("Here");
             RaycastHit hit;
             Ray ray = myCamera.ScreenPointToRay(mousePositionInput);
-            if (Physics.Raycast(ray, out hit,interactLayer))
+            if (Physics.Raycast(ray, out hit, interactLayer))
             {
                 //If the Object has the tag interactable for task objects and if the player is a villiage
                 if ((hit.transform.tag == "Interactable"))
                 {
                     Debug.Log("THe interactable tag works");
                     AU_Interactable temp = hit.transform.GetComponent<AU_Interactable>();
-                    temp.PlayMiniGame(); 
+                    temp.PlayMiniGame();
                 }
-            
+
                 //If the Object has the tag Vent and if the player is an imposter
-                if((hit.transform.tag == "Vent") && (isImposter == true))
+                if ((hit.transform.tag == "Vent") && (isImposter == true))
                 {
                     Debug.Log("THe vent tag works");
                     AU_Interactable temp = hit.transform.GetComponent<AU_Interactable>();
@@ -418,16 +492,16 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
                 }
 
                 //If the object has the tag Emergency
-                if((hit.transform.tag == "Emergency"))
+                if ((hit.transform.tag == "Emergency"))
                 {
                     Debug.Log("The emergency tag works");
                     AU_Interactable temp = hit.transform.GetComponent<AU_Interactable>();
                     temp.PlayMiniGame();
                 }
             }
-           
+
         }
-        
+
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -451,8 +525,9 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
     public void BecomeImposter(int ImposterNumber)
     {
         imposterAssigned = false;
-        if(playersInGame!=null){
-            foreach(AU_PlayerController p in playersInGame)
+        if (playersInGame != null)
+        {
+            foreach (AU_PlayerController p in playersInGame)
             {
                 if (p.isImposter)
                 {
@@ -460,49 +535,60 @@ public class AU_PlayerController : MonoBehaviour, IPunObservable
                 }
             }
         }
-            
-        if(!imposterAssigned && (PhotonNetwork.LocalPlayer == PhotonNetwork.PlayerList[ImposterNumber]))
+
+        if (!imposterAssigned && (PhotonNetwork.LocalPlayer == PhotonNetwork.PlayerList[ImposterNumber]))
         {
-            
+
             this.isImposter = true;
 
         }
     }
 
-    public void setImposterNumber(int ImposterNumber){
+    public void setImposterNumber(int ImposterNumber)
+    {
         imposterNumber = ImposterNumber;
     }
 
-    bool checkCrewmateVictory(){
+    bool checkCrewmateVictory()
+    {
         bool nonImposterExists = false;
-        foreach(AU_PlayerController p in playersInGame){
-            if(!p.isImposter){
+        foreach (AU_PlayerController p in playersInGame)
+        {
+            if (!p.isImposter)
+            {
                 nonImposterExists = true;
-                if(p.tasksCompleted!=2){
+                if (p.tasksCompleted != 2)
+                {
                     return false;
                 }
             }
         }
-        if(nonImposterExists){
+        if (nonImposterExists)
+        {
             return true;
         }
         return false;
     }
 
-    bool checkMafiaVictory(){
+    bool checkMafiaVictory()
+    {
         bool nonImposterExists = false;
-        foreach(AU_PlayerController p in playersInGame){
-            if(!p.isImposter){
+        foreach (AU_PlayerController p in playersInGame)
+        {
+            if (!p.isImposter)
+            {
                 nonImposterExists = true;
-                if(!p.isDead){
+                if (!p.isDead)
+                {
                     return false;
                 }
             }
         }
-        if(nonImposterExists){
+        if (nonImposterExists)
+        {
             return true;
         }
         return false;
     }
-    
+
 }
